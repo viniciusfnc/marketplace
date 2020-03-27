323757ac404b587c1ac4a2347afe46d0e26160fa
Vinicius Camargos <viniciusfnc@hotmail.com>
2020-03-27 08:37:46 +0000

    correção

--- app/helpers/card_helper.rb
+++ app/helpers/card_helper.rb
@@ -8,12 +8,13 @@ module CardHelper
         concat content_tag :p, report[:temporal], class: 'card-text'
         concat content_tag :p, report[:geographic], class: 'card-text'
         concat content_tag :p, number_to_currency(report[:base_price]) , class: 'card-text'
-        if report.users.empty?
+
+        user_report = UserReport.where(user_id: current_user, report_id: report).first
+        if user_report.nil?
           concat link_to "Adquirir", new_user_report_path(id: report.id), class: 'btn btn-primary'
         else
-          user_report = UserReport.where(user_id: current_user, report_id: report).first
           concat link_to "Revogar", user_report_path(user_report), method: :delete, class: 'btn btn-danger',
-                 data: { confirm: "Tem certeza que deseja revogar o acesso ao relatório '#{report.name}'?" } unless user_report.nil?
+                 data: { confirm: "Tem certeza que deseja revogar o acesso ao relatório '#{report.name}'?" }
         end
       end
     end
