4ad6d0b10b7f3078f8707aeca72026ba23a62014
Vinicius Camargos <viniciusfnc@hotmail.com>
2020-03-28 02:07:39 +0000

    melhorais layout

--- app/helpers/card_helper.rb
+++ app/helpers/card_helper.rb
@@ -2,22 +2,36 @@
 
 module CardHelper
   def card_report(report)
-    tag.div class: 'card', style: 'width: 18rem; margin-top: 20px;' do
-      tag.div class: 'card-body' do
-        concat content_tag(:h5, report[:name], class: 'card-title')
-        concat content_tag :p, report[:short_description], class: 'card-text'
-        concat content_tag :p, report[:temporal], class: 'card-text'
-        concat content_tag :p, report[:geographic], class: 'card-text'
-        concat content_tag :p, number_to_currency(report[:base_price]), class: 'card-text'
+    header_div = tag.div class: 'card-header', style: 'text-align: center;' do
+      concat content_tag :h5, report[:name], class: 'card-title'
+    end
 
-        user_report = UserReport.where(user_id: current_user, report_id: report).first
-        if user_report.nil?
-          concat link_to 'Adquirir', new_user_report_path(id: report.id), class: 'btn btn-primary'
-        else
-          concat link_to 'Revogar', user_report_path(user_report), method: :delete, class: 'btn btn-danger',
-                                                                   data: { confirm: "Tem certeza que deseja revogar o acesso ao relatório '#{report.name}'?" }
-        end
+    body_div = tag.div class: 'card-body', style: 'text-align: center;' do
+      concat content_tag :p, report[:short_description], class: 'card-text'
+
+      if report[:temporal]
+        concat content_tag :p, report[:temporal], class: 'card-text'
       end
+      if report[:geographic]
+        concat content_tag :p, report[:geographic], class: 'card-text'
+      end
+
+      concat content_tag :p, "Preço: #{number_to_currency(report[:base_price])}",
+                         class: 'card-text'
+    end
+
+    footer_div = tag.div class: 'card-footer', style: 'text-align: center;' do
+      user_report = UserReport.where(user_id: current_user, report_id: report).first
+      if user_report.nil?
+        concat link_to 'Adquirir', new_user_report_path(id: report.id), class: 'btn btn-primary'
+      else
+        concat link_to 'Revogar', user_report_path(user_report), method: :delete, class: 'btn btn-danger',
+                                                                 data: { confirm: "Tem certeza que deseja revogar o acesso ao relatório '#{report.name}'?" }
+      end
+    end
+
+    tag.div class: 'card', style: 'width: 18rem; margin-top: 20px;' do
+      header_div.concat(body_div).concat(footer_div).html_safe
     end
   end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Report < ApplicationRecord
-
   has_one_attached :sample_file
 
   has_and_belongs_to_many :kpis
